fbc4b0e36f7457a44d5184d2fb7a3b96ae16e961
zwbetz-gh <zwbetz@gmail.com>
2020-10-13 07:50:03 -0500

    Deploying git hash 3dc9152

--- js/index.js
+++ js/index.js
@@ -1,11 +1,29 @@
 (function () {
-  function clickEvent() {
+  function getColor() {
     const color = Please.make_color()[0];
-    console.log(color);
+    return color;
+  }
 
-    const resultEl = document.querySelector('#result');
-    resultEl.innerHTML = color;
-    resultEl.style.backgroundColor = color;
+  function clickEvent() {
+    const manyColorsEl = document.querySelector('#many_colors');
+
+    // Clear old colors
+    while (manyColorsEl.firstChild) {
+      manyColorsEl.removeChild(manyColorsEl.lastChild);
+    }
+
+    // Make new colors
+    for (let i = 0; i < 3; i++) {
+      const color = getColor();
+      console.log(color);
+
+      const colorEl = document.createElement('div');
+      colorEl.className = 'single_color';
+      colorEl.innerText = color;
+      colorEl.style.backgroundColor = color;
+
+      manyColorsEl.appendChild(colorEl);
+    }
   }
 
   function addEventListener() {
